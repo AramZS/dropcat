6857fb7ff66f47869a4806c00cd2ce0ac8960dfa
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-21 14:59:34 -0500

    various test plays

---
+++ personController.js
@@ -0,0 +1,7 @@
+function personController($scope) {
+    $scope.firstName = "John",
+    $scope.lastName = "Doe",
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+}
