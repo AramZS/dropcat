64595893ae4dceb40b7632146e54d7bc0ebaa6c6
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-22 00:37:45 -0500

    setup for further routes test

--- play/myUsers.js
+++ play/myUsers.js
@@ -1,4 +1,26 @@
-function userController($scope) {
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "play-home-part.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "play-about-part.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "play-home-part.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function ($scope /*, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
 	$scope.fName = '';
 	$scope.lName = '';
 	$scope.passw1 = '';
@@ -63,37 +85,4 @@ function userController($scope) {
     $scope.toggle = function() {
         $scope.myVar = !$scope.myVar;
     };
-
-}
-
-var app = angular.module('tutorialWebApp', [
-	'ngRoute'
-]);
-
-app.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when("/about", {templateUrl: "play-about-part.html", controller: "PageCtrl"})
-	.when("/", {templateUrl: "play-home-part.html", controller: "PageCtrl"});
-}]);
-
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-app.controller('userController', userController());
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
 });
