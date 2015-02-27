5260337442ad9e67c08771bc6489734284e8f2e3
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-27 16:32:27 -0500

    Basic retrieval and display, goooo!

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body ng-app="dropCat" ng-controller="dropController">
+
+  <label class="control-label">Category Slug:</label>
+  <div class="">
+    <input type="text" ng-model="catName">
+  </div>
+  <button class="btn btn-success" ng-disabled="error || incomplete" ng-click="getCatFeed(catName)">
+  	<span class="glyphicon glyphicon-save"></span> Save Changes
+  </button>
+
+<ul>
+  <li ng-repeat="x in posts">
+    {{ x.title + ', ' + x.ID }}
+  </li>
+</ul>
+
+
+<footer>
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+	<script src="lib/angularjs-wp-api/angular-wp-api.js"></script>
+	<script src="lib/ngResource/angular-resource.js"></script>
+	<script src="assets/js/app.js"></script>
+</footer>
+
+</body>
+</html>
