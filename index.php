93ef62536ecb0532bebb4d7836b44a7a10844088
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-27 16:59:23 -0500

    Add the new scripts (not min for now)

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html>
 
 <head>
-
+<link rel="stylesheet" href="stylesheets/app.css" />
+    
 </head>
 
 <body ng-app="dropCat" ng-controller="dropController">
@@ -24,8 +25,11 @@
 
 <footer>
 	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+	<script src="bower_components/jquery/dist/jquery.js"></script>
+	<script src="bower_components/foundation/js/foundation.js"></script>
 	<script src="lib/angularjs-wp-api/angular-wp-api.js"></script>
 	<script src="lib/ngResource/angular-resource.js"></script>
+	<script src="bower_components/modernizr/modernizr.js"></script>
 	<script src="assets/js/app.js"></script>
 	<script src="assets/js/dropController.js"></script>
 </footer>
