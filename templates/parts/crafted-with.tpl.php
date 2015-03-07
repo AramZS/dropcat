4fe7cdd797facb32ed57e0842c49c8b5fd144f5b
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-03-07 02:47:06 -0500

    Differing H settings.

--- templates/parts/crafted-with.tpl.php
+++ templates/parts/crafted-with.tpl.php
@@ -3,5 +3,5 @@
 ?>
 
 			<div class="large-3 medium-3 column">
-				<h6><?php echo $by; ?></h6>
+				<h<?php echo $h; ?>><a href="<?php echo $url; ?>"><?php echo $by; ?></a></h<?php echo $h; ?>>
 			</div>
